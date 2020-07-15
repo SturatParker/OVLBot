7b63f83f0bade2703fee869ca436d5a4b8b71c23
SturatParker <SturatParker@users.noreply.github.com>
2020-07-16 00:28:19 +0100

    Update ready.js

--- on/ready.js
+++ on/ready.js
@@ -1,3 +1,5 @@
-module.exports = () => {
+module.exports = (client) => {
+    const general = client.channels.find(channel => channel.name === "general")
+    general.send("I am alive")
     console.log('ready')
 }
