767b14db3fc807d2dd644b01c4eefcb46e01e12c
SturatParker <SturatParker@users.noreply.github.com>
2020-07-16 02:36:42 +0100

    Update message.js

--- on/message.js
+++ on/message.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const prefix = process.env.PREFIX
 
 module.exports = message => {
+    console.log(message.content)
     if (!message.content.startsWith(prefix))
         return
     message.content = message.content.slice(prefix.length)
-    console.log(message.content)
+    if (message.content = "ping") {
+        message.channel.send("pong")
+    }
 }
