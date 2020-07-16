defe2d6d579af30e0d76d1f970ace8e424a7ba24
SturatParker <SturatParker@users.noreply.github.com>
2020-07-17 00:38:41 +0100

    Update ready.js

--- on/ready.js
+++ on/ready.js
@@ -1,10 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
-module.exports = (client) => {
-    console.log(`Logged in as ${client.user.tag}`)
-    client.channels.fetch(process.env.LOG)
-    .then(channel => {
-        return channel.send("Ready")
-    })
-    .catch(console.error)
-}
+module.exports = client => {
+	console.log(`Logged in as ${client.user.tag}`);
+	if (process.env.MODE == "dev") return;
+	client.channels
+		.fetch(process.env.LOG)
+		.then(channel => {
+			return channel.send("Ready");
+		})
+		.catch(console.error);
+};
