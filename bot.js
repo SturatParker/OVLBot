3656c3cd51616758dd1ddad543982a6f73b8f4cd
SturatParker <SturatParker@users.noreply.github.com>
2020-07-17 00:38:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
+const { connect } = require('./db/db')
 const { Client } = require("discord.js");
 const message = require("./on/message");
 const ready = require("./on/ready");
 const messageReactionAdded = require("./on/messageReactionAdd");
 const bot = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
-bot.login(process.env.DISCORD_TOKEN).catch(console.log);
+connect()
+.then(()=> {
+	return bot.login(process.env.DISCORD_TOKEN)
+})
+.catch(console.log);
 
 bot.on("ready", function() {
 	ready(this);
