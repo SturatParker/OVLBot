5684cb52914317f7e20bb0dc933c874ea91a775e
SturatParker <SturatParker@users.noreply.github.com>
2020-07-17 01:29:37 +0100

    Update messageReactionAdd.js

--- on/messageReactionAdd.js
+++ on/messageReactionAdd.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 module.exports = async (messageReaction, user) => {
-	console.log("messageReactionAdded");
 	if (messageReaction.partial) {
 		try {
 			await messageReaction.fetch();
@@ -11,9 +10,13 @@ module.exports = async (messageReaction, user) => {
 		}
 	}
 
-	if (messageReaction.emoji.name != "👍")
-		return;
-	messageReaction.message.channel.send(
-		"Thanks for your vote! I won't remember it because I don't have a database yet!"
-	);
+	if (messageReaction.emoji.name == "👍") {
+		return user
+			.send(
+				"Thanks for your vote! I won't remember it because I don't have a database yet!"
+			)
+			.then(() => {
+				return messageReaction.remove();
+			});
+	}
 };
