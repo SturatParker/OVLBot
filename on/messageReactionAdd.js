1fc3774a23309e5c7fc5ec16de50692bfb69072d
SturatParker <SturatParker@users.noreply.github.com>
2020-07-18 01:40:24 +0100

    Reject duplicate votes or vote over limit

--- on/messageReactionAdd.js
+++ on/messageReactionAdd.js
@@ -1,26 +1,61 @@
 require("dotenv").config();
-const { createVote } = require("../db/db");
+const { getVotes, createVote } = require("../db/db");
 
-module.exports = async (messageReaction, user) => {
+const acknowledgeVote = (user, voteMessage) => {
+	return user.send(`Thanks for voting for ***${voteMessage}***`);
+};
+
+const rejectVote = (user, voteMessage, reason) => {
+	return user.send(
+		`Sorry, we couldn't register your vote for ***${voteMessage}*** because ${reason ||
+			"unspecified reason"}`
+	);
+};
+
+const processRequest = (votes, user, messageReaction) => {
+	const voteLim = process.env.VOTE_LIMIT;
+	const alreadyVoted = votes.some(
+		vote => vote.message == messageReaction.message.id
+	);
+	const voteText = messageReaction.message.content;
+	if (alreadyVoted) {
+		return rejectVote(user, voteText, "you have already voted for it");
+	}
+	if (votes.length >= voteLim) {
+		return rejectVote(
+			user,
+			voteText,
+			`you have cast the maximum number of votes (${voteLim})`
+		);
+	}
+	return createVote({
+		user: user.id,
+		message: messageReaction.message.id
+	}).then(acknowledgeVote(user, messageReaction.message.content));
+};
+
+const completePartial = message => {
+	if (message.partial) {
+		return message.fetch();
+	} else {
+		return Promise.resolve(message);
+	}
+};
+
+module.exports = (messageReaction, user) => {
 	if (messageReaction.message.channel.id != process.env.CHANNEL) {
-		return;
+		return Promise.resolve();
 	}
 	if (messageReaction.emoji.name != "👍") {
-		return;
+		return Promise.resolve();
 	}
-	if (messageReaction.partial) {
-		try {
-			await messageReaction.fetch();
-		} catch (error) {
-			console.log("Something went wrong fetching a partial message", error);
-			return;
-		}
-	}
-	return createVote({ user: user.id, message: messageReaction.message.id })
-		.then(res => {
-			return user.send(
-				`Thanks for voting for ${messageReaction.message.content}`
-			);
+
+	completePartial(messageReaction)
+		.then(() => {
+			return getVotes({ user: user.id });
+		})
+		.then(votes => {
+			return processRequest(votes, user, messageReaction);
 		})
 		.then(() => {
 			return messageReaction.remove();
