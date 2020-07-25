460910baffe49a053a29146766b28fdfb57df11a
SturatParker <SturatParker@users.noreply.github.com>
2020-07-25 18:22:01 +0100

    Update winner.js

--- commands/winner.js
+++ commands/winner.js
@@ -1,38 +1,39 @@
 require("dotenv").config();
-const { getVotes } = require("../db/db");
-const tallyReducer = (acc, cur) => {
-	prev = acc.find(x => {
-		return x.message == cur.message;
-	});
-	if (prev) {
-		prev.count++;
-	} else {
-		acc.push({
-			message: cur.message,
-			count: 1
-		});
-	}
-	return acc;
-};
+const { getAllItems } = require("../db/db");
 
 module.exports = (message, ...args) => {
 	// if (!message.member.hasPermission("MANAGE_GUILD")) {
 	// 	return Promise.resolve();
 	// }
-	return getVotes().then(votes => {
-		if (!votes.length) {
-			return message.channel.send("No votes have been cast");
+	return getAllItems().then(items => {
+		votedItems = items
+			.filter(item => item.voterIds.length)
+			.sort((a, b) => {
+				return b.voterIds.length - a.voterIds.length;
+			});
+		if (!votedItems.length) {
+			return message.channel.send("No votes found");
 		}
-		let tally = votes.reduce(tallyReducer, []).sort((a, b) => {
-			a.count - b.count;
-		});
-		let max = tally[0];
-        return message.guild.channels.cache.get(process.env.CHANNEL).messages.fetch(max.message)
-            .then(m => {
-			return message.channel.send(
-				`\`${m.content}\` won with ${max.count}/${votes.length} votes`
-			);
-		});
+		totalVotes = votedItems.reduce((acc, cur) => {
+			return acc + cur.voterIds.length;
+		}, 0);
+		winners = votedItems.filter(
+			item => item.voterIds.length == votedItems[0].voterIds.length
+		);
+		isTie = winners.length > 1;
+		const embed = {
+			title: "Poll winners",
+			description: `A total of ${totalVotes} votes were cast this round. With ${
+				winners[0].voterIds.length
+			} votes the ${isTie ? "tied winners are" : "winner is"}:`,
+			color: 1353797,
+			fields: winners.map(item => ({
+				name: item.messageContent,
+				value: `Submitted by: <@${item.submittedById}>`
+			}))
+		};
+		return message.channel.send({embed});
+		let a = new String()
+		a.slice()
 	});
-	return Promise.resolve();
 };
