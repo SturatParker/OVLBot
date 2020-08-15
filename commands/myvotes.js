bb15b5c07ca50bad692ba34d96739fb1c62a8c1a
SturatParker <SturatParker@users.noreply.github.com>
2020-08-15 18:28:55 +0100

    Update myvotes.js

--- commands/myvotes.js
+++ commands/myvotes.js
@@ -2,24 +2,26 @@ require("dotenv").config();
 const { itemsByVoterId } = require("../db/db");
 const { MessageEmbed } = require("discord.js");
 const { color } = require("../config");
-const voteLim = process.env.VOTE_LIMIT
+const voteLim = process.env.VOTE_LIMIT;
 
 module.exports = {
 	name: "myvotes",
 	description: "Who have I voted for?",
 	execute: (message, ...args) => {
 		embed = new MessageEmbed();
-		return itemsByVoterId(message.author.id)
-			.then(res => {
-				embed
-					.setColor(color.success)
-					.setTitle("Your votes")
-                    .setDescription(`You have cast ${res.length} votes, and have ${voteLim - res.length} vote remaining`);
-                embed.fields = res.map(item=> ({
-                    name: item.messageContent,
-                    value: `Submitted by: <@${item.submittedById}>`
-                }))
-				return message.channel.send({ embed });
-			})
+		return itemsByVoterId(message.author.id).then(res => {
+			embed
+				.setColor(color.success)
+				.setTitle("Your votes")
+				.setDescription(
+					`You have cast ${res.length} votes, and have ${voteLim -
+						res.length} vote remaining`
+				);
+			embed.fields = res.map(item => ({
+				name: item.messageContent,
+				value: `Submitted by: <@${item.submittedById}>`
+			}));
+			return message.author.send({ embed });
+		});
 	}
 };
